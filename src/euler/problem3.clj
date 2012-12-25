54edc052f0579a70489623234b37646441746b98
Glenn <gleenn@gmail.com>
2012-12-25 00:24:15 -0800

    wip problem 3

--- src/euler/problem3.clj
+++ src/euler/problem3.clj
@@ -2,12 +2,14 @@
 
 (defn prime-factors [x]
   (doto #{}
-    (doseq [factor (range 2 (Math/sqrt x))]
+    (for [factor (range 2 (Math/sqrt x))]
       (if (= 0 (mod x factor))
         (merge factors factor)
         )
       )
     )
   )
+
+(test )
 
 (prime-factors 10)
