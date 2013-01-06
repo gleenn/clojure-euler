e26cc6bc0b813c335d1f1d5d4dbea11d0579be75
Glenn <gleenn@gmail.com>
2013-01-05 22:10:00 -0800

    finished fake hackercup problem A -- finally :/

--- src/hackercup/problemA.clj
+++ src/hackercup/problemA.clj
@@ -1,27 +1,33 @@
 (ns hackercup.problemA)
 
-(use '[clojure.string :only (join split)])
+(use '[clojure.string :only (split)])
 (use '[clojure.java.io :only (reader)])
 
 
-;(with-open [rdr (clojure.java.io/reader "../../data/problemA/input.txt")]
-;  (if-let [[x & xs] (line-seq rdr)]
-;    (prn x "foo" xs)))
-
 (defn gen [seed limit]
-  (take-while #(< % limit)
-    (iterate #(mod (* 1337 %) 31337))
-    seed))
+  (take limit
+    (iterate
+      #(mod (* 1337 %) 31337)
+      seed)))
 
-(defn dist [])
+;(defn dist [^long x ^long y]
+(defn dist [^long x ^long y]
+  (let [dx (Math/abs (- x 12345))
+        dy (Math/abs (- y 6789))]
+    [(Math/sqrt (+ (* dx dx) (* dy dy))) x y]))
 
 (defn solve [a b c]
   (let [[a b c] (map (fn [param] (Integer/parseInt param)) [a b c])]
     (let [xs (gen a c)
           ys (gen b c)]
-      )))
+      (let [hundredth-closest (nth (sort (map #(apply dist %) (map vector xs ys))) 99)]
+        (apply + (next hundredth-closest))))))
 
-(prn (map
-       (fn [params] (apply solve params))
-       (map (fn [line] (split line #"\s+"))
-         (next (line-seq (reader "../../data/problemA/input.txt"))))))
+(def do-problem
+  (map (fn [params] (apply solve params))
+    (map (fn [line] (split line #"\s+"))
+      (next (line-seq (reader "../../data/problemA/input.txt"))))))
+
+(time (doseq [result
+      (map-indexed (fn [i result] (str "Case #" (str (inc i)) ":" (str result))) do-problem)]
+  (println result)))
