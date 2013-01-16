adce459bfe5d7e5546c0ca509a35bf9ac210ee75
Glenn <gleenn@gmail.com>
2013-01-16 12:13:09 -0800

    WIP problem 7

--- src/euler/problem7.clj
+++ src/euler/problem7.clj
@@ -1,9 +1,28 @@
 (ns euler.problem7)
 
-(defn is-prime [n]
-  true)
+(defn divides? [num divisor] (= (rem num divisor) 0))
 
-(defn find-nth-prime [n]
-  (some (fn [num] (if (is-prime num) num false)) (iterate inc 1)))
+(defn find-nth-prime [nth]
+  (loop [x 2
+         num-primes 1]
+    (if (= num-primes nth)
+      x
+;      (if (some (partial divides? x) (range 2 (Math/ceil (Math/sqrt x))))
+      (if (some (partial divides? x) (range 2 x))
+        (recur (inc x) num-primes)
+        (recur (inc x) (inc num-primes))))))
 
-(prn (find-nth-prime 10001))
+
+(assert (= (divides? 4 2) true))
+(assert (= (divides? 4 3) false))
+
+(assert (= (some (partial divides? x) (range 2 x) )
+
+(prn (find-nth-prime 3))
+(assert (= (find-nth-prime 1) 2))
+(assert (= (find-nth-prime 2) 3))
+(assert (= (find-nth-prime 3) 5))
+(assert (= (find-nth-prime 4) 7))
+(assert (= (find-nth-prime 5) 11))
+
+;(prn (find-nth-prime 10001))
